d71e7596cfd07c205a8036e076958d0ae2e52984
Filip Wieladek <Wattos@gmail.com>
2011-10-21 14:36:40 +0200

    Migrated to VS 2010 Added support for svn 1.7 Note: Only the 64bit installer works!

--- src/JiraSVN.Plugin/SvnProperties.cs
+++ src/JiraSVN.Plugin/SvnProperties.cs
@@ -13,8 +13,8 @@
  */
 #endregion
 using System;
-using System.Collections.Generic;
 using System.IO;
+using SharpSvn;
 
 namespace CSharpTest.Net.JiraSVN.Plugin
 {
@@ -26,6 +26,7 @@ namespace CSharpTest.Net.JiraSVN.Plugin
 
 		public string Search(string path, bool recurseUp, string propName)
 		{
+
 			if (!String.IsNullOrEmpty(_base))
 				path = Path.Combine(_base, path);
 
@@ -35,86 +36,16 @@ namespace CSharpTest.Net.JiraSVN.Plugin
 					throw new FileNotFoundException(path);
 				path = Path.GetDirectoryName(path);
 			}
-			
-			return SearchPath(new DirectoryInfo(path), recurseUp, propName);
-		}
 
-		private static string SearchPath(DirectoryInfo di, bool recurseUp, string propName)
-		{
-			DirectoryInfo[] svn = di.GetDirectories(".svn");
-			if( svn.Length == 0 )
-				svn = di.GetDirectories("_svn", SearchOption.TopDirectoryOnly);
-
-			foreach (DirectoryInfo diSvn in svn)
-			{
-				string value;
-				FileInfo[] dirProps = diSvn.GetFiles("dir-props");
-				if (dirProps.Length == 0)
-					dirProps = diSvn.GetFiles("dir-prop-base");
-
-				if (dirProps.Length == 1 && null != (value = ReadSetting(dirProps[0], propName)))
-					return value;
-			}
-
-			if (recurseUp && di.Parent != null)
-				return SearchPath(di.Parent, true, propName);
-
-			return null;
-		}
-
-		private static string ReadSetting(FileInfo file, string propName)
-		{
-			Dictionary<string, string> props = ReadSettings(file);
-			
-			string value;
-			if (props.TryGetValue(propName, out value))
-				return value;
-			return null;
-		}
-
-		private static Dictionary<string, string> ReadSettings(FileInfo file)
-		{
-			Dictionary<string, string> properties = new Dictionary<string, string>();
-			try
-			{
-				int count;
-				char[] buffer;
-				string line, key, value;
-
-				using (TextReader rdr = file.OpenText())
-				{
-					while (null != (line = rdr.ReadLine()))
-					{
-						if (line.StartsWith("K ") == false)
-							continue;
-
-						count = int.Parse(line.Substring(2));
-						buffer = new char[count];
-						if (count != rdr.Read(buffer, 0, count))
-							break;
-
-						key = new String(buffer);
-						rdr.ReadLine();//value followed by eol
-						if (null == (line = rdr.ReadLine()))
-							break;
-
-						if (line.StartsWith("V ") == false)
-							continue;
-
-						count = int.Parse(line.Substring(2));
-						buffer = new char[count];
-						if (count != rdr.Read(buffer, 0, count))
-							break;
-
-						value = new String(buffer);
-						properties.Add(key, value);
-						
-						rdr.ReadLine();
-					}
-				}
-			}
-			catch { }
-			return properties;
+            using (var client = new SvnClient()) {
+                string result;
+                Guid guid;
+                do {
+                    client.TryGetProperty(SvnTarget.FromString(path), propName, out result);
+                    path = Directory.GetParent(path).FullName;
+                } while (result == null && recurseUp && client.TryGetRepositoryId(path, out guid));
+                return result ?? string.Empty;
+            }
 		}
 	}
 }
